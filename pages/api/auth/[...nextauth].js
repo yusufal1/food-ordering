4aa50287d4c9fabd3bec0258d89150cecfb4ad30
Yusuf AL <yusufal5558@gmail.com>
2023-07-03 18:09:48 +0300

    auth

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,18 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import clientPromise from "../../../util/mongo";
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  adapter: MongoDBAdapter(clientPromise),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+  pages: {
+    signIn: "/auth/login",
+  },
+});
